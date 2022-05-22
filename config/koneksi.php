ab043b26492b411797ffe2a4eb4f62ada9981a3a
Yanu TriN <yanutrinugrahati@gmail.com>
2022-05-22 21:55:17 +0700

    upload pertama

---
+++ config/koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+$hostname 	= "localhost"; 
+$username 	= "root";
+$password 	= "yanu1998";
+$database 	= "simpeg";
+
+$koneksi=mysqli_connect($hostname,$username,$password,$database);
+
+
+
+?>
